218218bb29d2bdb3874676630f4713b5d059b711
shuu11 <shuu1104main@gmail.com>
2023-01-12 22:52:01 +0900

    change

--- zzz-environment-variables-app/pages/index.tsx
+++ zzz-environment-variables-app/pages/index.tsx
@@ -112,7 +112,7 @@ const IndexPage = () => (
 // only run in Node.js. Check the terminal to see the environment variables
 export async function getStaticProps() {
   // Using the variables below in the browser will return `undefined`. Next.js doesn't
-  // expose environment variables unless they start with `NEXT_PUBLIC_`
+  // expose environment variables unlessy they start with `NEXT_PUBLIC_`
   console.log('[Node.js only] ENV_VARIABLE:', process.env.ENV_VARIABLE)
   console.log(
     '[Node.js only] ENV_LOCAL_VARIABLE:',
